ada97cfabd53962188e7a8c06eaa007338f747d2
lkelsall <kelsall.lewis@gmail.com>
2021-06-27 17:14:11 +0100

    add Twitter API POST statuses/update request

--- bot.js
+++ bot.js
@@ -21,29 +21,45 @@ const city_name =
 
 superagent
   .get(
-    `https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=${city_name}%20parks%20and%20gardens&inputtype=textquery&fields=name,formatted_address,place_id&key=${process.env.PLACES_API_KEY}`
+    `https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=${city_name}%20parks%20and%20gardens&inputtype=textquery&fields=place_id&key=${process.env.PLACES_API_KEY}`
   )
   .then((response) => {
     const { place_id } = response.body.candidates[0];
     return superagent.get(
-      `https://maps.googleapis.com/maps/api/place/details/json?place_id=${place_id}&fields=photo&key=${process.env.PLACES_API_KEY}`
+      `https://maps.googleapis.com/maps/api/place/details/json?place_id=${place_id}&fields=name,formatted_address,photo&key=${process.env.PLACES_API_KEY}`
     );
   })
   .then((response) => {
-    const photoReqs = response.body.result.photos.map((photo) => {
+    const photos = response.body.result.photos.slice(0, 4);
+    const photoReqs = photos.map((photo) => {
       return superagent.get(
         `https://maps.googleapis.com/maps/api/place/photo?photoreference=${photo.photo_reference}&maxheight=1000&maxwidth=1000&key=${process.env.PLACES_API_KEY}`
       );
     });
-    return Promise.all(photoReqs);
+    return Promise.all([...photoReqs, response.body.result]);
   })
   .then((responses) => {
-    responses.splice(4);
+    const cityDetails = responses.splice(responses.length - 1, 1)[0];
     const uploadReqs = responses.map((photo) => {
       return twitterClient.post("/media/upload", { media: photo.body });
     });
-    return Promise.all(uploadReqs);
+    return Promise.all([...uploadReqs, cityDetails]);
   })
   .then((responses) => {
-    console.log(responses);
+    const cityDetails = responses.splice(responses.length - 1, 1)[0];
+    const media_ids = responses
+      .map((response) => {
+        return response.media_id_string;
+      })
+      .join(",");
+    return twitterClient.post("statuses/update", {
+      status: `${cityDetails.name},\n${cityDetails.formatted_address}`,
+      media_ids: media_ids,
+    });
+  })
+  .then((tweet) => {
+    console.log(tweet);
+  })
+  .catch((err) => {
+    console.log(err);
   });
