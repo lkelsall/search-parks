77428c884364e9eaa79820a55b66b99e3c126144
lkelsall <kelsall.lewis@gmail.com>
2021-06-27 15:06:09 +0100

    expand list of candidate cities

--- cities.js
+++ cities.js
@@ -107,4 +107,557 @@ exports.cities = [
     formatted_address: "St Petersburg, Russia",
     name: "St Petersburg",
   },
+  {
+    formatted_address: "Nairobi, Kenya",
+    name: "Nairobi",
+  },
+  {
+    formatted_address: "Cape Town, South Africa",
+    name: "Cape Town",
+  },
+  {
+    formatted_address: "Lagos, Nigeria",
+    name: "Lagos",
+  },
+  {
+    formatted_address: "Johannesburg, South Africa",
+    name: "Johannesburg",
+  },
+  {
+    formatted_address: "Accra, Ghana",
+    name: "Accra",
+  },
+  {
+    formatted_address: "Addis Ababa, Ethiopia",
+    name: "Addis Ababa",
+  },
+  {
+    formatted_address: "Dar es Salaam, Tanzania",
+    name: "Dar es Salaam",
+  },
+  {
+    formatted_address: "Cairo, Cairo Governorate, Egypt",
+    name: "Cairo",
+  },
+  {
+    formatted_address: "Luanda, Angola",
+    name: "Luanda",
+  },
+  {
+    formatted_address: "Kinshasa, Democratic Republic of the Congo",
+    name: "Kinshasa",
+  },
+  {
+    formatted_address: "Kigali, Rwanda",
+    name: "Kigali",
+  },
+  {
+    formatted_address: "Dakar, Senegal",
+    name: "Dakar",
+  },
+  {
+    formatted_address: "Durban, South Africa",
+    name: "Durban",
+  },
+  {
+    formatted_address: "Abidjan, Côte d'Ivoire",
+    name: "Abidjan",
+  },
+  {
+    formatted_address: "Kampala, Uganda",
+    name: "Kampala",
+  },
+  {
+    formatted_address: "Lusaka, Zambia",
+    name: "Lusaka",
+  },
+  {
+    formatted_address: "Pretoria, South Africa",
+    name: "Pretoria",
+  },
+  {
+    formatted_address: "Casablanca, Morocco",
+    name: "Casablanca",
+  },
+  {
+    formatted_address: "Abuja, Nigeria",
+    name: "Abuja",
+  },
+  {
+    formatted_address: "Maputo, Mozambique",
+    name: "Maputo",
+  },
+  {
+    formatted_address: "Mogadishu, Somalia",
+    name: "Mogadishu",
+  },
+  {
+    formatted_address: "Windhoek, Namibia",
+    name: "Windhoek",
+  },
+  {
+    formatted_address: "Algiers, Sidi M'Hamed, Algeria",
+    name: "Algiers",
+  },
+  {
+    formatted_address: "Brazzaville, Republic of the Congo",
+    name: "Brazzaville",
+  },
+  {
+    formatted_address: "Ouagadougou, Burkina Faso",
+    name: "Ouagadougou",
+  },
+  {
+    formatted_address: "Bamako, Mali",
+    name: "Bamako",
+  },
+  {
+    formatted_address: "Antananarivo, Madagascar",
+    name: "Antananarivo",
+  },
+  {
+    formatted_address: "Buenos Aires, Argentina",
+    name: "Buenos Aires",
+  },
+  {
+    formatted_address: "Rio de Janeiro, State of Rio de Janeiro, Brazil",
+    name: "Rio de Janeiro",
+  },
+  {
+    formatted_address: "Santiago, Santiago Metropolitan Region, Chile",
+    name: "Santiago",
+  },
+  {
+    formatted_address: "Lima, Peru",
+    name: "Lima",
+  },
+  {
+    formatted_address: "Bogotá, Bogota, Colombia",
+    name: "Bogotá",
+  },
+  {
+    formatted_address: "Quito, Ecuador",
+    name: "Quito",
+  },
+  {
+    formatted_address: "São Paulo, State of São Paulo, Brazil",
+    name: "São Paulo",
+  },
+  {
+    formatted_address: "Montevideo, Montevideo Department, Uruguay",
+    name: "Montevideo",
+  },
+  {
+    formatted_address: "La Paz, Bolivia",
+    name: "La Paz",
+  },
+  {
+    formatted_address: "Medellín, Medellin, Antioquia, Colombia",
+    name: "Medellín",
+  },
+  {
+    formatted_address: "Cartagena, Cartagena Province, Bolivar, Colombia",
+    name: "Cartagena",
+  },
+  {
+    formatted_address: "Cusco, Peru",
+    name: "Cusco",
+  },
+  {
+    formatted_address: "Caracas, Capital District, Venezuela",
+    name: "Caracas",
+  },
+  {
+    formatted_address: "Brasilia - Federal District, Brazil",
+    name: "Brasilia",
+  },
+  {
+    formatted_address: "Salvador - State of Bahia, Brazil",
+    name: "Salvador",
+  },
+  {
+    formatted_address: "Asunción, Paraguay",
+    name: "Asunción",
+  },
+  {
+    formatted_address: "Guayaquil, Ecuador",
+    name: "Guayaquil",
+  },
+  {
+    formatted_address: "Belo Horizonte, State of Minas Gerais, Brazil",
+    name: "Belo Horizonte",
+  },
+  {
+    formatted_address: "Cali, Valle del Cauca, Colombia",
+    name: "Cali",
+  },
+  {
+    formatted_address: "Fortaleza - State of Ceará, Brazil",
+    name: "Fortaleza",
+  },
+  {
+    formatted_address: "Curitiba, State of Paraná, Brazil",
+    name: "Curitiba",
+  },
+  {
+    formatted_address: "Valparaíso, Chile",
+    name: "Valparaíso",
+  },
+  {
+    formatted_address: "Recife - State of Pernambuco, Brazil",
+    name: "Recife",
+  },
+  {
+    formatted_address: "Manaus, State of Amazonas, Brazil",
+    name: "Manaus",
+  },
+  {
+    formatted_address: "Sucre, Bolivia",
+    name: "Sucre",
+  },
+  {
+    formatted_address: "Belém - State of Pará, Brazil",
+    name: "Belém",
+  },
+  {
+    formatted_address: "Córdoba, Cordoba, Argentina",
+    name: "Córdoba",
+  },
+  {
+    formatted_address: "New York, NY, USA",
+    name: "New York",
+  },
+  {
+    formatted_address: "Toronto, ON, Canada",
+    name: "Toronto",
+  },
+  {
+    formatted_address: "Los Angeles, CA, USA",
+    name: "Los Angeles",
+  },
+  {
+    formatted_address: "Montreal, QC, Canada",
+    name: "Montreal",
+  },
+  {
+    formatted_address: "Chicago, IL, USA",
+    name: "Chicago",
+  },
+  {
+    formatted_address: "Mexico City, CDMX, Mexico",
+    name: "Mexico City",
+  },
+  {
+    formatted_address: "Boston, MA, USA",
+    name: "Boston",
+  },
+  {
+    formatted_address: "San Francisco, CA, USA",
+    name: "San Francisco",
+  },
+  {
+    formatted_address: "Philadelphia, PA, USA",
+    name: "Philadelphia",
+  },
+  {
+    formatted_address: "Vancouver, BC, Canada",
+    name: "Vancouver",
+  },
+  {
+    formatted_address: "Seattle, WA, USA",
+    name: "Seattle",
+  },
+  {
+    formatted_address: "Houston, TX, USA",
+    name: "Houston",
+  },
+  {
+    formatted_address: "Washington, DC, USA",
+    name: "Washington",
+  },
+  {
+    formatted_address: "Phoenix, AZ, USA",
+    name: "Phoenix",
+  },
+  {
+    formatted_address: "Havana, Cuba",
+    name: "Havana",
+  },
+  {
+    formatted_address: "Santo Domingo, Dominican Republic",
+    name: "Santo Domingo",
+  },
+  {
+    formatted_address: "Guatemala City, Guatemala",
+    name: "Guatemala City",
+  },
+  {
+    formatted_address: "Ottawa, ON, Canada",
+    name: "Ottawa",
+  },
+  {
+    formatted_address: "Port-au-Prince, Haiti",
+    name: "Port-au-Prince",
+  },
+  {
+    formatted_address: "Las Vegas, NV, USA",
+    name: "Las Vegas",
+  },
+  {
+    formatted_address: "San Diego, CA, USA",
+    name: "San Diego",
+  },
+  {
+    formatted_address: "Monterrey, Nuevo Leon, Mexico",
+    name: "Monterrey",
+  },
+  {
+    formatted_address: "Portland, OR, USA",
+    name: "Portland",
+  },
+  {
+    formatted_address: "Austin, TX, USA",
+    name: "Austin",
+  },
+  {
+    formatted_address: "Detroit, MI, USA",
+    name: "Detroit",
+  },
+  {
+    formatted_address: "Denver, CO, USA",
+    name: "Denver",
+  },
+  {
+    formatted_address: "Guadalajara, Jalisco, Mexico",
+    name: "Guadalajara",
+  },
+  {
+    formatted_address: "Port Moresby, Papua New Guinea",
+    name: "Port Moresby",
+  },
+  {
+    formatted_address: "Suva, Fiji",
+    name: "Suva",
+  },
+  {
+    formatted_address: "Nouméa, New Caledonia",
+    name: "Nouméa",
+  },
+  {
+    formatted_address: "Honiara, Solomon Islands",
+    name: "Honiara",
+  },
+  {
+    formatted_address: "Canberra ACT 2601, Australia",
+    name: "Canberra",
+  },
+  {
+    formatted_address: "Wellington, New Zealand",
+    name: "Wellington",
+  },
+  {
+    formatted_address: "Pape'ete 98714, French Polynesia",
+    name: "Pape'ete",
+  },
+  {
+    formatted_address: "Auckland, New Zealand",
+    name: "Auckland",
+  },
+  {
+    formatted_address: "Sydney NSW, Australia",
+    name: "Sydney",
+  },
+  {
+    formatted_address: "Melbourne VIC, Australia",
+    name: "Melbourne",
+  },
+  {
+    formatted_address: "Perth WA, Australia",
+    name: "Perth",
+  },
+  {
+    formatted_address: "Brisbane QLD, Australia",
+    name: "Brisbane",
+  },
+  {
+    formatted_address: "Adelaide SA, Australia",
+    name: "Adelaide",
+  },
+  {
+    formatted_address: "Christchurch, New Zealand",
+    name: "Christchurch",
+  },
+  {
+    formatted_address: "Nuku'alofa, Tonga",
+    name: "Nuku'alofa",
+  },
+  {
+    formatted_address: "Apia, Samoa",
+    name: "Apia",
+  },
+  {
+    formatted_address: "Port Vila, Vanuatu",
+    name: "Port Vila",
+  },
+  {
+    formatted_address: "Hobart TAS 7000, Australia",
+    name: "Hobart",
+  },
+  {
+    formatted_address: "Pago Pago, Eastern District, American Samoa",
+    name: "Pago Pago",
+  },
+  {
+    formatted_address: "Wollongong NSW 2500, Australia",
+    name: "Wollongong",
+  },
+  {
+    formatted_address: "Newcastle NSW, Australia",
+    name: "Newcastle",
+  },
+  {
+    formatted_address: "Darwin NT, Australia",
+    name: "Darwin",
+  },
+  {
+    formatted_address: "Geelong VIC 3220, Australia",
+    name: "Geelong",
+  },
+  {
+    formatted_address: "Lae, Papua New Guinea",
+    name: "Lae",
+  },
+  {
+    formatted_address: "Cairns QLD, Australia",
+    name: "Cairns",
+  },
+  {
+    formatted_address: "Dunedin, New Zealand",
+    name: "Dunedin",
+  },
+  {
+    formatted_address: "Gold Coast QLD, Australia",
+    name: "Gold Coast",
+  },
+  {
+    formatted_address: "Hong Kong",
+    name: "Hong Kong",
+  },
+  {
+    formatted_address: "Seoul, South Korea",
+    name: "Seoul",
+  },
+  {
+    formatted_address: "Beijing, China",
+    name: "Beijing",
+  },
+  {
+    formatted_address: "Bangkok, Thailand",
+    name: "Bangkok",
+  },
+  {
+    formatted_address: "Tokyo, Japan",
+    name: "Tokyo",
+  },
+  {
+    formatted_address: "Mumbai, Maharashtra, India",
+    name: "Mumbai",
+  },
+  {
+    formatted_address: "Shanghai, China",
+    name: "Shanghai",
+  },
+  {
+    formatted_address: "Jakarta, Indonesia",
+    name: "Jakarta",
+  },
+  {
+    formatted_address: "Taipei City, Taiwan",
+    name: "Taipei City",
+  },
+  {
+    formatted_address:
+      "Kuala Lumpur, Federal Territory of Kuala Lumpur, Malaysia",
+    name: "Kuala Lumpur",
+  },
+  {
+    formatted_address: "Hanoi, Hoàn Kiếm, Hanoi, Vietnam",
+    name: "Hanoi",
+  },
+  {
+    formatted_address: "Manila, Metro Manila, Philippines",
+    name: "Manila",
+  },
+  {
+    formatted_address: "Ho Chi Minh City, Vietnam",
+    name: "Ho Chi Minh City",
+  },
+  {
+    formatted_address: "Guangzhou, Guangdong Province, China",
+    name: "Guangzhou",
+  },
+  {
+    formatted_address: "Bengaluru, Karnataka, India",
+    name: "Bengaluru",
+  },
+  {
+    formatted_address: "Kolkata, West Bengal, India",
+    name: "Kolkata",
+  },
+  {
+    formatted_address: "Karachi, Karachi City, Sindh, Pakistan",
+    name: "Karachi",
+  },
+  {
+    formatted_address: "Dhaka, Bangladesh",
+    name: "Dhaka",
+  },
+  {
+    formatted_address: "Chennai, Tamil Nadu, India",
+    name: "Chennai",
+  },
+  {
+    formatted_address: "Tianjin, China",
+    name: "Tianjin",
+  },
+  {
+    formatted_address: "Shenzhen, Guangdong Province, China",
+    name: "Shenzhen",
+  },
+  {
+    formatted_address: "Yangon, Myanmar (Burma)",
+    name: "Yangon",
+  },
+  {
+    formatted_address: "Phnom Penh, Cambodia",
+    name: "Phnom Penh",
+  },
+  {
+    formatted_address: "Kyoto, Japan",
+    name: "Kyoto",
+  },
+  {
+    formatted_address: "Osaka, Japan",
+    name: "Osaka",
+  },
+  {
+    formatted_address: "Xi'an, Shaanxi, China",
+    name: "Xi'an",
+  },
+  {
+    formatted_address: "Chongqing, China",
+    name: "Chongqing",
+  },
+  {
+    formatted_address: "Jerusalem, Israel",
+    name: "Jerusalem",
+  },
+  {
+    formatted_address: "Jerusalem, Israel",
+    name: "Jerusalem",
+  },
+  {
+    formatted_address: "Yerevan, Armenia",
+    name: "Yerevan",
+  },
 ];
