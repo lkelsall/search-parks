e4a98911a7d5d5bb88849cbd2ecfe979b4895dc2
lkelsall <kelsall.lewis@gmail.com>
2021-06-27 14:51:43 +0100

    create list of candidate cities

--- bot.js
+++ bot.js
@@ -1,5 +1,8 @@
 const superagent = require("superagent");
 const path = require("path");
+const { cities } = require("./cities");
+
+console.log(cities);
 
 const ENV = process.env.NODE_ENV || "development";
 
@@ -9,8 +12,8 @@ require("dotenv").config({
 
 const city_name = "Copenhagen, Denmark";
 
-superagent
-  .get(
-    `https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=${city_name}%20parks%20and%20gardens&inputtype=textquery&fields=name,formatted_address&key=${process.env.PLACES_API_KEY}`
-  )
-  .then((response) => console.log(response.body));
+// superagent
+//   .get(
+//     `https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=${city_name}%20parks%20and%20gardens&inputtype=textquery&fields=name,formatted_address&key=${process.env.PLACES_API_KEY}`
+//   )
+//   .then((response) => console.log(response.body));
---
+++ cities.js
@@ -0,0 +1,110 @@
+exports.cities = [
+  {
+    formatted_address: "Berlin, Germany",
+    name: "Berlin",
+  },
+  {
+    formatted_address: "Rome, Metropolitan City of Rome, Italy",
+    name: "Rome",
+  },
+  {
+    formatted_address: "London, UK",
+    name: "London",
+  },
+  {
+    formatted_address: "Paris, France",
+    name: "Paris",
+  },
+  {
+    formatted_address: "Amsterdam, Netherlands",
+    name: "Amsterdam",
+  },
+  {
+    formatted_address: "Lisbon, Portugal",
+    name: "Lisbon",
+  },
+  {
+    formatted_address: "Prague, Czechia",
+    name: "Prague",
+  },
+  {
+    formatted_address: "Copenhagen, Denmark",
+    name: "Copenhagen",
+  },
+  {
+    formatted_address: "Barcelona, Spain",
+    name: "Barcelona",
+  },
+  {
+    formatted_address: "Vienna, Austria",
+    name: "Vienna",
+  },
+  {
+    formatted_address: "Madrid, Spain",
+    name: "Madrid",
+  },
+  {
+    formatted_address: "Stockholm, Sweden",
+    name: "Stockholm",
+  },
+  {
+    formatted_address: "Athens, Greece",
+    name: "Athens",
+  },
+  {
+    formatted_address: "Budapest, Hungary",
+    name: "Budapest",
+  },
+  {
+    formatted_address: "İstanbul, Turkey",
+    name: "İstanbul",
+  },
+  {
+    formatted_address: "Venice, Metropolitan City of Venice, Italy",
+    name: "Venice",
+  },
+  {
+    formatted_address: "Dublin, Ireland",
+    name: "Dublin",
+  },
+  {
+    formatted_address: "Tallinn, Estonia",
+    name: "Tallinn",
+  },
+  {
+    formatted_address: "Munich, Germany",
+    name: "Munich",
+  },
+  {
+    formatted_address: "Warsaw, Poland",
+    name: "Warsaw",
+  },
+  {
+    formatted_address: "Helsinki, Finland",
+    name: "Helsinki",
+  },
+  {
+    formatted_address: "Edinburgh, UK",
+    name: "Edinburgh",
+  },
+  {
+    formatted_address: "Seville, Spain",
+    name: "Seville",
+  },
+  {
+    formatted_address: "Hamburg, Germany",
+    name: "Hamburg",
+  },
+  {
+    formatted_address: "Kraków, Poland",
+    name: "Kraków",
+  },
+  {
+    formatted_address: "Florence, Metropolitan City of Florence, Italy",
+    name: "Florence",
+  },
+  {
+    formatted_address: "St Petersburg, Russia",
+    name: "St Petersburg",
+  },
+];
